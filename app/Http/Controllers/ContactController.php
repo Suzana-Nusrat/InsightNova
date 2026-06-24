d4f06d7247956dac6594c8f76e19ad585b8da335
Nusrat Suzana <109594461+Suzana-Nusrat@users.noreply.github.com>
2026-06-24 09:52:36 +0600

    Add files via upload

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:3',
+            'email' => 'required|email',
+            'subject' => 'required|min:3',
+            'message' => 'required|min:10',
+        ]);
+
+        Contact::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'subject' => $request->subject,
+            'message' => $request->message,
+        ]);
+
+        return back()->with('success', 'Your message has been sent successfully!');
+    }
+}
